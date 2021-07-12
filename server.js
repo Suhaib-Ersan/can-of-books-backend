b5d1e7d34f376a5d4e9ccbe2bc3c150a5630814c
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-07-12 18:58:21 +0300

    basic working server

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
-
-const app = express();
-app.use(cors());
-
+require("dotenv").config();
+const express = require("express");
+const server = express();
+const cors = require("cors");
+server.use(cors());
+const jwt = require("jsonwebtoken");
+const jwksClient = require("jwks-rsa");
 const PORT = process.env.PORT || 3001;
+const axios = require("axios");
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/favoritebooks', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/test', (request, response) => {
 
-  // TODO: 
+
+const kittySchema = new mongoose.Schema({
+  name: String,
+  breed: String
+});
+
+
+
+
+
+server.get('/test', (req, res) => {
+  res.status(200).send('server received request on /test')
+})
+
+
+server.get("/test", (req, res) => {
+  // TODO:
   // STEP 1: get the jwt from the headers
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+});
 
-})
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
