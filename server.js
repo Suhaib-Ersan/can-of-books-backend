30d04b7cc129a6d2f3d45b044291c7d5513702fd
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-07-12 19:01:32 +0300

    added 404 route

--- server.js
+++ server.js
@@ -28,13 +28,10 @@ server.get('/test', (req, res) => {
 })
 
 
-server.get("/test", (req, res) => {
-  // TODO:
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
-});
 
+
+server.get('/*', (req, res) => {
+  res.status(404).send('404: not found')
+})
 
 server.listen(PORT, () => console.log(`listening on ${PORT}`));
